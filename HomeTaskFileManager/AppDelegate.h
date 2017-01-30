9d3bc6362f2b548f567438268833c61a5db0b14e
Vladimir Eryshev <vladimir.eryshev@wukkerteam.com>
2017-01-30 21:26:38 +0300

    Initial Commit

---
+++ HomeTaskFileManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeTaskFileManager
+//
+//  Created by VLAD on 30/01/2017.
+//  Copyright © 2017 Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomeTaskFileManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeTaskFileManager
+//
+//  Created by VLAD on 30/01/2017.
+//  Copyright © 2017 Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
